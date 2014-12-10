ce863ff9a5f9fe67edf55b85c2bf4375bdb5c9d9
Idris Raja <idris.raja@gmail.com>
2014-12-10 12:28:45 +0800

    Assembly multiplication

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -1,9 +1,40 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
+// Multiplies R0 and R1 and stores the result in R2.  // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.) 
 
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// loop setup
+@0
+D=M
 
-// Put your code here.
+@a
+M=D
+
+@1
+D=M
+@b
+M=D
+
+@i
+M=0 // i=0
+
+@2
+M=0 // location for the product
+
+// while loop
+(LOOP)
+    @i
+    D=M
+    @a
+    D=D-M
+    @END
+    D;JEQ
+
+    @b
+    D=M
+
+    @2
+    M=D+M
+
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(END)
