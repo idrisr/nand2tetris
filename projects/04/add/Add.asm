5691f9bdd66c393cd9fb945dcca41328dc4b7445
Idris Raja <idris.raja@gmail.com>
2014-12-10 10:08:08 +0800

    fuck yeah! Got add to run successfully. My first Assembly code evah

--- projects/04/add/Add.asm
+++ projects/04/add/Add.asm
@@ -1,4 +1,4 @@
-// Multiplies R0 and R1 and stores the result in R2.  // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.) 
+// Adds R0 and R1 and stores the result in R2.  // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.) 
 
 @R0
 D=M
