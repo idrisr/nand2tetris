dd2f59f94bfc668a1e34e4b8bd9763c1cd8fdb3b
Idris Raja <idris.raja@gmail.com>
2014-12-11 22:28:52 +0800

    not the most elegant, but it works!

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,24 +1,52 @@
-@16384
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel. When no key is pressed, the
+// program clears the screen, i.e. writes "white" in every pixel.
+
+// constant value for 16384
+@SCREEN
 D=A
 
-@screen_start
-M=D
-
 @i
-M=D // i=screen_start
+M=D 
+
+@SCREEN
+D=A
 
 @24575
 D=A
-
 @screen_end
 M=D
 
 (LOOP)
+    @SCREEN
+    D=A
+
+    @i
+    M=D 
+
+    @SCREEN
+    D=A
+
+    @24575
+    D=A
+    @screen_end
+    M=D
+
+    @KBD
+    D=M
+
+    @TOGGLEOFF
+    D;JEQ
+    @TOGGLEON
+
+// set screen to black
+(TOGGLEON)
     @i
     D=M
     @screen_end
     D=M-D
-    @END
+    @LOOP
     D;JLT
 
     @i    // i=i+1 (with new i left in D)
@@ -28,10 +56,23 @@ M=D
     @i
     M=M+1
 
-    @LOOP
-    0;JMP
-(END)
+    @TOGGLEON
+    D;JMP
 
-// This is the key to moving the address pointer around
-// A=M
-// M=D
+(TOGGLEOFF)
+    @i
+    D=M
+    @screen_end
+    D=M-D
+    @LOOP
+    D;JLT
+
+    @i    // i=i+1 (with new i left in D)
+    A=M
+    M=0
+
+    @i
+    M=M+1
+
+    @TOGGLEOFF
+    D;JMP
