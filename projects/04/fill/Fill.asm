b71b8a38999d5570bfb4524f8ecc130d53ba8b00
Idris Raja <idris.raja@gmail.com>
2014-12-11 12:49:36 +0800

    get loop to work for setting screen

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -16382,7 +16382,40 @@ M=D
 @24573
 M=D
 @24574
+@16384
+D=A
+
+@screen_start
 M=D
+
+@i
+M=D // i=screen_start
+
 @24575
+D=A
+
+@screen_end
 M=D
-0;JMP
+
+(LOOP)
+    @i
+    D=M
+    @screen_end
+    D=M-D
+    @END
+    D;JLT
+
+    @i    // i=i+1 (with new i left in D)
+    A=M
+    M=-1
+
+    @i
+    M=M+1
+
+    @LOOP
+    0;JMP
+(END)
+
+// This is the key to moving the address pointer around
+// A=M
+// M=D
