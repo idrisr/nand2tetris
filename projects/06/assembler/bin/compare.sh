c76fe8ed6f3cd9ca90a503129f6aff5c3c136e68
Idris Raja <idris.raja@gmail.com>
2014-12-16 19:06:42 +0800

    script for auto-diffing known binary and the ones I created

--- projects/06/assembler/bin/compare.sh
+++ projects/06/assembler/bin/compare.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
 rm *hack > /dev/null
+
+echo 'Chekcing MaxL...'
 python ../assembler/parser.py ../../max/MaxL.asm > Max.hack
 diff -yb --suppress-common-lines Max.hack ../../max/MaxL.hack
 
+echo 'Checking AddL...'
 python ../assembler/parser.py ../../add/Add.asm > Add.hack
 diff -yb --suppress-common-lines Add.hack ../../add/Add.hack
 
+echo 'Checking PongL...'
 python ../assembler/parser.py ../../pong/PongL.asm > Pong.hack 
 diff -yb --suppress-common-lines Pong.hack ../../pong/PongL.hack
 
+echo 'Checking RectL...'
 python ../assembler/parser.py ../../rect/RectL.asm > Rect.hack 
 diff -yb --suppress-common-lines Rect.hack ../../rect/RectL.hack
