2626a09030f84a76b28b08a21717ebdf227fcdfe
Idris Raja <idris.raja@gmail.com>
2014-12-18 18:14:31 +0800

    move CommandType into VMParser

--- projects/software_stack/vm/vm/vm_parser.py
+++ projects/software_stack/vm/vm/vm_parser.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 import sys
+import re
 from parser import Parser
+from command_type import VMCommandType
+
 
 class VMParser(Parser):
     def __init__(self, file_name):
@@ -18,14 +21,41 @@ class VMParser(Parser):
         finally:
             self.file.close()
 
+        self.vmct = VMCommandType()
 
     def command_type(self):
-        pass
+        arith_commands = ['add', 'sub', 'neg', 'eq', 'gt', 'lt', 'and', 'or', 'not']
+        arith_re = r'|'.join(['^%s$' % _ for _ in arith_commands])
+        funcs = {
+            'C_ARITHMETIC' : lambda x : re.match(arith_re          , x , re.I) ,
+            'C_PUSH'       : lambda x : re.match(r'^push\s.*$'     , x , re.I) ,
+            'C_POP'        : lambda x : re.match(r'^pop\s.*$'      , x , re.I) ,
+            'C_LABEL'      : lambda x : re.match(r'^label\s.*$'    , x , re.I) ,
+            'C_GOTO'       : lambda x : re.match(r'^goto\s.*$'     , x , re.I) ,
+            'C_IF'         : lambda x : re.match(r'^if\s.*$'       , x , re.I) ,
+            'C_FUNCTION'   : lambda x : re.match(r'^function\s.*$' , x , re.I) ,
+            'C_RETURN'     : lambda x : re.match(r'^return\s.*$'   , x , re.I) ,
+            'C_CALL'       : lambda x : re.match(r'^call\s.*$'     , x , re.I)
+        }
 
+        self.cur_command_type = None
+        for k, v in funcs.items():
+            if v(self.current_command):
+                self.cur_command_type = k
+                break
+
+        if self.cur_command_type is None:
+            print '%s\tinvalid command' % (self.current_command, )
+            sys.exit(1)
 
     def arg1(self):
-        pass
-
+        if self.cur_command_type == 'C_RETURN':
+            print '%s\tInvalid Command Type' % (self.cur_command_type, )
+            sys.exit(1)
+        elif self.cur_command_type == 'C_ARITHMETIC':
+            self.arg1 = self.current_command
+        else:
+            self.arg1 = self.current_command.split()[1]
 
     def arg2(self):
         pass
--- projects/software_stack/vm/vm/vm_test_parser.py
+++ projects/software_stack/vm/vm/vm_test_parser.py
@@ -22,3 +22,74 @@ class TestVMParser(TestCase):
         with self.assertRaises(SystemExit) as cm:
             self.parser.__init__(file_name)
         self.assertEqual(cm.exception.code, 1)
+
+    def test_arg1(self):
+        command = 'push constant 21'
+        self.parser.current_command = command
+        self.parser.command_type()
+        self.parser.arg1()
+        self.assertEqual('constant', self.parser.arg1)
+
+    def test_push(self):
+        """ test recognizing push commands """
+        self.parser.current_command = 'push constant 7'
+        self.parser.command_type()
+        self.assertEqual(self.parser.cur_command_type, 'C_PUSH')
+
+        self.parser.current_command = 'PUSH CONSTANT 7'
+        self.parser.command_type()
+        self.assertEqual(self.parser.cur_command_type, 'C_PUSH')
+
+        self.parser.current_command = 'POP CONSTANT 7'
+        self.parser.command_type()
+        self.cur_command_type = None
+        self.assertNotEqual(self.parser.cur_command_type, 'C_PUSH')
+
+    def test_pop(self):
+        """ test recognizing pop commands """
+        self.parser.current_command = 'pop argument 7'
+        self.parser.command_type()
+        self.assertEqual(self.parser.cur_command_type, 'C_POP')
+
+        self.parser.command = 'pOp arguemnt 8'
+        self.parser.command_type()
+        self.assertEqual(self.parser.cur_command_type, 'C_POP')
+
+        self.parser.current_command = 'push CONSTANT 7'
+        self.parser.command_type()
+        self.cur_command_type = None
+        self.assertNotEqual(self.parser.cur_command_type, 'C_POP')
+
+    def test_add_arithmetic(self):
+        """ test recognizing add commands """
+        self.parser.current_command = 'add'
+        self.parser.command_type()
+        self.assertEqual(self.parser.cur_command_type, 'C_ARITHMETIC')
+
+        self.parser.current_command = 'notadd'
+        with self.assertRaises(SystemExit) as cm:
+            self.parser.command_type()
+        self.assertEqual(cm.exception.code, 1)
+
+        self.parser.current_command = 'pop this'
+        self.parser.command_type()
+        self.assertNotEqual(self.parser.cur_command_type, 'C_ARITHMETIC')
+
+    def test_sub_arithmetic(self):
+        """ test recognizing sub commands """
+        self.parser.current_command = 'sub'
+        self.parser.command_type()
+        self.assertEqual(self.parser.cur_command_type, 'C_ARITHMETIC')
+
+        self.parser.current_command = 'push that'
+        self.parser.command_type()
+        self.assertNotEqual(self.parser.cur_command_type, 'C_ARITHMETIC')
+
+
+    def test_invalid_command(self):
+        """ test for invalid command """
+        self.parser.current_command = 'subbie'
+
+        with self.assertRaises(SystemExit) as cm:
+            self.parser.command_type()
+        self.assertEqual(cm.exception.code, 1)
